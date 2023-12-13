f47c01060380bea0e54682e63acf6622106e9117
EduardoLoz12 <eduardolozadav@hotmail.com>
2023-12-13 21:14:32 +0100

    Proyecto Finito
    Yo terminé el codigo puto, revisalo

--- Edu/Trabajo_Final_v1.1.0.py
+++ Edu/Trabajo_Final_v1.1.0.py
@@ -1,13 +1,12 @@
 from datetime import date, datetime
 import streamlit as st
 import pandas as pd
-import mplfinance as mpf
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import krakenex
 from pykrakenapi import KrakenAPI
 import time
-import pandas_ta as ta
+
 
 #---------------Data---------------------------------------------
 
@@ -49,25 +48,15 @@ with c3:
 
 st.markdown('---')
 
-st.sidebar.subheader('Settings')
-st.sidebar.caption('Adjust charts settings and then press apply')
+st.sidebar.subheader('Moving Average Selector')
+st.sidebar.caption('Select the days of rollback and then press apply')
 
 
 
-with st.sidebar.form('settings_form'):
-    show_nontrading_days = st.checkbox('Show non-trading days', True)
+with st.sidebar.form('Movin Average Selector'):
 
-    mystyle = mpf.make_mpf_style(rc={'axes.labelsize': 'medium'}, style_name="My Style")
-
-    chart_styles = [
-        'default', 'binance', 'blueskies', 'brasil',
-        'charles', 'checkers', 'classic', 'yahoo', mystyle
-       ]
-    chart_style = st.selectbox('Chart style', options=chart_styles, index=chart_styles.index('default'))
-
-    mav1 = st.number_input('Mav 1', min_value=3, max_value=30, value=3, step=1)
-    mav2 = st.number_input('Mav 2', min_value=6, max_value=30, value=6, step=1)
-    mav3 = st.number_input('Mav 3', min_value=9, max_value=30, value=9, step=1)
+    mav1 = st.number_input('Mav 1', min_value=3, max_value=50, value=3, step=1)
+    mav2 = st.number_input('Mav 2', min_value=6, max_value=50, value=6, step=1)
 
     st.form_submit_button('Apply')
 
@@ -79,15 +68,28 @@ print(data)
 data.ta.stoch(high='high', low='low', k=14, d=3, append=True)
 
 fig = go.Figure()
-fig = make_subplots(rows=2, cols=1)
+fig = make_subplots(rows=3, cols=1,
+                    shared_xaxes=True,
+                    row_heights=[0.3, 0.8, 0.3],
+                    )
+
+fig.for_each_xaxis(lambda x: x.update(showticklabels=True))
+
 fig.add_trace(go.Candlestick(
     x=data['date'],
     open=data['open'],
     high=data['high'],
     low=data['low'],
     close=data['close'],
-    name='Price'
-    )
+    name='Price',
+    ), row=2, col=1,
+)
+
+fig.add_trace(go.Bar(
+    x=data['date'],
+    y=data['volume'],
+    name='Volume',),
+    row=1, col=1,
 )
 
 epoch_timestamp = int(unixtime)
@@ -107,34 +109,62 @@ fig.append_trace(
     go.Scatter(
         x=data.index,
         y=data['STOCHk_14_3_3'],
-        line=dict(width=2),
+        line=dict(width=2, color='pink'),
         name='fast',
-    ), row=2, col=1  #  <------------ lower chart %d
+    ), row=3, col=1  #  <------------ lower chart %d
 )
 fig.append_trace(
     go.Scatter(
         x=data.index,
         y=data['STOCHd_14_3_3'],
-        line=dict(width=2),
+        line=dict(width=2, color='purple'),
         name='slow'
-    ), row=2, col=1 #  <------------ lower chart
+    ), row=3, col=1 #  <------------ lower chart
 )
+#Creating moving Average
 
-# Extend our y-axis a bit
-fig.update_yaxes(range=[-10, 110], row=2, col=1)
+data['MA1'] = data.close.rolling(mav1).mean()
+data['MA2'] = data.close.rolling(mav2).mean()
+
+
+fig.append_trace(
+    go.Scatter(
+        x=data.index,
+        y=data.MA1,
+        line=dict(color='white', width=1),
+        name=f'MA {mav1}'),
+        row=2, col=1)
+
+fig.append_trace(
+    go.Scatter(
+        x=data.index,
+        y=data.MA2,
+        line=dict(color='grey', width=1),
+        name=f'MA {mav2}'),
+        row=2, col=1)
+
+
 
 
 # Add overbought/oversold
-fig.add_hline(y=20, col=1, row=2, line_color='#336699', line_width=2, line_dash='dash') # type: ignore
-fig.add_hline(y=80, col=1, row=2, line_color='#336699', line_width=2, line_dash='dash') # type: ignore
+fig.add_hline(y=20, col=1, row=3, line_color='#336699', line_width=2, line_dash='dash') # type: ignore
+fig.add_hline(y=80, col=1, row=3, line_color='#336699', line_width=2, line_dash='dash') # type: ignore
+
+# Add MAVs
+
 
 fig.update_layout(
-    title='Graficas Probando',
-    yaxis_title= 'Price',
-    width=800,  # Set the width to 800 pixels
-    height=600)
+        title={
+        'text': f'Graficas de {symbol}',
+        'font': {'size': 25}},
+        width=800, height=800)
+
+fig.update_yaxes(title_text = "Volume", row=1, col=1)
+fig.update_yaxes(title_text = "OHLC", row=2, col=1)
+fig.update_yaxes(title_text = "Stocastic", row=3, col=1)
 
 st.plotly_chart(fig)
-st.subheader('Data')
-
-st.write(data)
+if show_data:
+    st.markdown('---')
+    st.subheader('Data')
+    st.dataframe(data)
